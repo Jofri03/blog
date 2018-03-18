30068abfe1a6004ce65dc796b5b337a7796fd2eb
unknown <985801@CS.MUM.EDU>
2018-03-18 12:20:23 -0500

    unit test for DataService.java

--- src/model/Blog.java
+++ src/model/Blog.java
@@ -3,7 +3,41 @@ package model;
 import java.util.Date;
 
 public class Blog {
+	int id;
 	String title;
 	String content;
 	Date createdDate;
+	int userId;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
 }
--- src/service/DataService.java
+++ src/service/DataService.java
@@ -1,5 +1,11 @@
 package service;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Blog;
+
 // The Service for Blog
 public class DataService {
 
@@ -7,69 +13,76 @@ public class DataService {
     public DataService() {
       try {
         Class.forName("com.mysql.jdbc.Driver");
-        this.conn = DriverManager.getConnection("jdbc:mysql://45.77.144.61:3306/blogdb?userSSL=false", "root", "123456");
+        this.conn = DriverManager.getConnection("jdbc:mysql://108.61.89.50:3306/blogdb?useSSL=false", "cun", "123456");
       } catch (Exception ex) {
         System.out.println(ex);
       }
     }
 
     // Fetch all the blogs at once.
-    public List<Blog> getAll() {
+    public List<Blog> getAll() throws SQLException {
       List<Blog> blogs = new ArrayList<>();
       Statement stmt = conn.createStatement();
       String strSelect = "select * from blog";
       ResultSet rset = stmt.executeQuery(strSelect);
       while (rset.next()) {
         Blog temp = new Blog();
-        temp.title = rset.getString("title");
-        temp.content = rset.getString("content");
-        System.out.println(title + ", " + content);
-        blogs.append(temp);
+        int blogId = rset.getInt("id");
+        String title = rset.getString("title");
+        String content = rset.getString("content");
+        temp.setId(blogId);
+        temp.setTitle(title);
+        temp.setContent(content);
+        blogs.add(temp);
       }
       return blogs;
     }
 
     // Fetch one blog by id.
-    public Blog getOne(int id) {
+    public Blog getOne(int id) throws SQLException {
       Blog blog = new Blog();
       Statement stmt = conn.createStatement();
       String strSelect = "select * from blog where id = " + id;
       ResultSet rset = stmt.executeQuery(strSelect);
       rset.next();
-      blog.title = rset.getString("title");
-      blog.content = rset.getString("content");
+      int blogId = rset.getInt("id");
+      String title = rset.getString("title");
+      String content = rset.getString("content");
+      blog.setId(blogId);
+      blog.setTitle(title);
+      blog.setContent(content);
       return blog;
     }
 
     // Add a new blog.
-    public boolean add(Blog blog) {
-      Statement stmt = conn.createStatement();
-      String strSelect = String.format("insert into blog(title, content, user_id) " +
-                         "values({0}, {1}, {2})", blog.title, blog.content, blog.user_id);
-
-      stmt.executeUpdate(strSelect, Statement.RETURN_GENERATED_KEYS);
-      ResultSet rs = stmt.getGeneratedKeys();
-      rs.next();
-      int pk = rs.getInt(1);
-      return pk > 0;
+    public boolean add(Blog blog) throws SQLException {
+      String SQL = "INSERT INTO blog(title, content, user_id) VALUES(?, ?, ?)";
+      PreparedStatement pstmt = conn.prepareStatement(SQL);
+      pstmt.setString(1, blog.getTitle());
+      pstmt.setString(2, blog.getContent());
+      pstmt.setInt(3, blog.getUserId());
+      int count = pstmt.executeUpdate();
+      return count > 0;
     }
 
     // Delete a blog by id.
-    public boolean delete(init id) {
+    public boolean delete(int id) throws SQLException {
       String SQL = "DELETE FROM blog WHERE id = ? ";
       PreparedStatement pstmt = conn.prepareStatement(SQL);
-      pstmt.setString(1, id);
+      pstmt.setInt(1, id);
+      System.out.println(pstmt);
       pstmt.executeUpdate();
       return true;
     }
 
     // Modify a blog.
-    public boolean modify(Blog b) {
-      String SQL = "UPDATE blog SET title = ?, set content = ? where id = ?";
+    public boolean modify(Blog blog) throws SQLException {
+      String SQL = "UPDATE blog SET title = ?, content = ? where id = ?";
       PreparedStatement pstmt = conn.prepareStatement(SQL);
-      pstmt.setString(1, blog.title);
-      pstmt.setString(2, blog.content);
-      pstmt.setString(3, blog.id);
+      pstmt.setString(1, blog.getTitle());
+      pstmt.setString(2, blog.getContent());
+      pstmt.setInt(3, blog.getId());
+      System.out.println(pstmt);
       pstmt.executeUpdate();
       return true;
     }
